253c8ee0b6732a44beaf68c729a8520f633c98dd
Natnael Biruk <natnaelbiruk3@gmail.com>
2023-01-04 21:09:41 +0300

    redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
-import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/scenes/navBar/index.jsx
@@ -0,0 +1,5 @@
+const NavPage = () => {
+  return <div>Nav Page</div>;
+};
+
+export default NavPage;
